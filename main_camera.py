83ddf85b7a725ca152259737c9d4be497c2b51b8
Array29 <azhaisanbek@gmail.com>
2024-06-17 16:48:35 +0500

    splitting point cloud to two according picked points and turning it face to face

--- main_camera.py
+++ main_camera.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
+import open3d as o3d
 from Camera import Camera
 def main():
 
     camera = Camera()
     color, depth = camera.start_stream()
     pcd = camera.capturePointCloud(color, depth)
+    camera.split_point_cloud(pcd)
     # pcd1 = camera.capture_PC_depth(depth)
     # camera.dist_between_2points(pcd)
     # camera.dist_camera_point(pcd)
